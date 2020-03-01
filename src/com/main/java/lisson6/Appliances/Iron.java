f959b1fbf9024839a021f460d17235de68229d4d
Sasha <sashawormsik@gmail.com>
2020-03-01 20:33:11 +0300

    Task 24

---
+++ src/com/main/java/lisson6/Appliances/Iron.java
@@ -0,0 +1,28 @@
+package com.main.java.lisson6.Appliances;
+
+public class Iron extends SmallHouseholdAppliances {
+    private boolean steamer;
+    private int numberOfModes;
+
+    public Iron(String company, int powerConsumption, boolean stationary, boolean portable, boolean steamer, int numberOfModes) {
+        super(company, powerConsumption, stationary, portable);
+        this.steamer = steamer;
+        this.numberOfModes = numberOfModes;
+    }
+
+    public boolean isSteamer() {
+        return steamer;
+    }
+
+    public void setSteamer(boolean steamer) {
+        this.steamer = steamer;
+    }
+
+    public int getNumberOfModes() {
+        return numberOfModes;
+    }
+
+    public void setNumberOfModes(int numberOfModes) {
+        this.numberOfModes = numberOfModes;
+    }
+}
